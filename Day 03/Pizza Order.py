fc39f8615be1ed4c4b8f03ffe6d2b5431c4cc960
SUMITH SAI <km0686@srmist.edu.in>
2023-05-21 00:29:33 +0530

    Pizza Order Practise

---
+++ Day 03/Pizza Order.py
@@ -0,0 +1,30 @@
+#Exercise 4 - Pizza Order Practise (SUMITH SAI KORABOINA)
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+bill=0
+#Write your code below this line 👇
+if size=="S":
+    bill+=15
+    if add_pepperoni=="Y":
+        bill+=2
+elif size=="M":
+    bill+=20
+    if add_pepperoni=="Y":
+        bill+=3
+elif size=="L":
+    bill+=25
+    if add_pepperoni=="Y":
+        bill+=3
+
+if extra_cheese=="Y":
+    bill+=1
+
+
+print("Your final bill is: $"+str(bill))
+
+#ALL THE BEST FROM SUMITH SAI KORABOINA :)
