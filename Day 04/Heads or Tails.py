593c8ac11e490fd29f207d58e1fe638241675d19
SUMITH SAI <km0686@srmist.edu.in>
2023-06-05 03:54:06 +0530

    Create Heads or Tails.py

---
+++ Day 04/Heads or Tails.py
@@ -0,0 +1,15 @@
+#Heads or Tails (SUMITH SAI KORABOINA)
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+toss=random.randint(0,1)
+#print(type(toss))
+if toss==0:
+    print("Heads")
+elif toss==1:
+    print("Tails")
+
+#ALL THE BEST FROM SUMITH SAI KORABOINA
