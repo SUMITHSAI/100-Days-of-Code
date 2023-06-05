576378e27ede841b75c798a7913a747143bf999b
SUMITH SAI <km0686@srmist.edu.in>
2023-06-06 00:38:01 +0530

    Day 04

---
+++ Day 04/Treasure Map.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+#TREASURE MAP {SUMITH SAI KORABOINA} 
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+vertical=int(position[0])
+horizontal=int(position[1])
+map[horizontal-1][vertical-1]="X"
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+#ALL THE BEST 
+#THANKS FOR VISITING
+#SUMITH SAI KORABOINA
