4911405a06d812e6a83eb2ceb88be4ce0036ed15
SUMITH SAI <km0686@srmist.edu.in>
2023-06-10 23:59:00 +0530

    Day 10

---
+++ Day 10/Days in month.py
@@ -0,0 +1,38 @@
+#Days in month
+
+#Day 10
+
+#SUMITH SAI KORABOINA
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year,month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+  result=is_leap(year)
+  if result==False:
+      no_of_days=month_days[month-1]
+      print(no_of_days)
+  elif result==True:
+      if month==2:
+          print("29")
+      elif month!=2 :
+          no_of_days=month_days[month-1]
+          print(no_of_days)
+
+
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days_in_month(year, month)
+# print(days)
