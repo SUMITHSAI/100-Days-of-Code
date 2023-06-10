b7ad759f8479f366bbf8925520c067261efa2c02
SUMITH SAI <km0686@srmist.edu.in>
2023-06-10 15:03:34 +0530

    Day 09

---
+++ Day 09/Grading Program.py
@@ -0,0 +1,33 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+#Grading Program
+
+#Day 09
+
+#Sumith Sai Koraboina
+
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades={}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for key in student_scores:
+    score=student_scores[key]
+    if score>90:
+        student_grades[key]="Outstanding"
+    elif score>80:
+        student_grades[key]="Exceeds Expectations"
+    elif score>70:
+        student_grades[key]="Acceptable"
+    elif score<=70:
+        student_grades[key]="Fail"
+
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
